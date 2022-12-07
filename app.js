4fd4f911889976d7f554e6cae411cd4b33053487
simran1002 <simran2131056@akgec.ac.in>
2022-12-07 20:02:44 +0530

    updated files

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
+const cookieParser = require('cookie-parser');
 const express = require('express')
-
+const passport = require('passport')
 const app = express()
+const PORT = process.env.PORT || 5000;
 
-app.set("view engine","ejs")
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(cookieParser());
 
 
 app.get('/',(req,res) => {
     res.render("pages/index")
 })
 
-app.listen(5000,() => {
-    console.log("App is running at Port 5000")
+app.get('/login',(req,res) => {
+    res.render('login');
+})
+
+app.post('/login',(req,res) => {
+    let token = req.body.token;
+
+    console.log(token);
+})
+
+app.listen(PORT,() => {
+    console.log(`App is running at Port ${PORT}`);
 })
