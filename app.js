72e9a33a3a6eef6d2e754e99a860d00f27a69f93
simran1002 <simran2131056@akgec.ac.in>
2022-12-11 23:30:13 +0530

    updated files

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const passport = require('passport')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+//google auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '277985285628-560of7ar2drp5dl57go9sphegm3int6n.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -11,7 +16,7 @@ app.use(cookieParser());
 
 
 app.get('/',(req,res) => {
-    res.render("pages/index")
+    res.render("index")
 })
 
 app.get('/login',(req,res) => {
@@ -22,7 +27,31 @@ app.post('/login',(req,res) => {
     let token = req.body.token;
 
     console.log(token);
+    async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    console.log(payload)
+  }
+  verify()
+  .then(()=>{
+    res.cookie('session-token',token );
+    res.send('success');
+  }).
+  catch(console.error);
 })
+
+app.get('/profile', (req, res)=>{
+    let user = req.user;
+    res.render('profile', {user});
+})
+
+app.get('/protectedroute', (req,res)=>{
+    res.render('protectedroute.ejs');
+})
+
 
 app.listen(PORT,() => {
     console.log(`App is running at Port ${PORT}`);
