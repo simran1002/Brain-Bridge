ba913a618a017eba6ab2c12b8e6d4121a12eeb0d
simran1002 <simran2131056@akgec.ac.in>
2022-12-13 18:02:38 +0530

    updated files

--- app.js
+++ app.js
@@ -1,90 +1,59 @@
+require('dotenv').config()
+
 const cookieParser = require('cookie-parser');
 const express = require('express')
 const passport = require('passport')
 const app = express()
 const PORT = process.env.PORT || 5000
-const { connectMongoose, User } = require("./source/db/connection.js");
-connectMongoose();
+const session = require('express-session');
+require("./passport-setup");
 
 
+
+// After you declare "app"
+app.use(session({ secret: 'melody hensley is my spirit animal' }));
+// Authentication configuration
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'bla bla bla' 
+  }));
 //google auth
 const {OAuth2Client} = require('google-auth-library');
+const { Passport } = require('passport');
 const CLIENT_ID = '277985285628-560of7ar2drp5dl57go9sphegm3int6n.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 
 
 app.set("view engine","ejs");
+
+app.use(passport.initialize())
+
+app.use(passport.session())
+
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.get('/',(req,res) => {
-    res.render("index")
+app.get('/',(req,res)=> {
+    res.render("pages/index")
 })
 
-app.get('/login',(req,res) => {
+app.get('/pages/login',(req,res) => {
     res.render('login');
 })
 
-app.post('/login',(req,res) => {
-    let token = req.body.token;
-
-    console.log(token);
-    async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-    });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-    console.log(payload)
-  }
-  verify()
-  .then(()=>{
-    res.cookie('session-token',token );
-    res.send('success');
-  }).
-  catch(console.error);
+app.get('/success',(req,res) => {
+    res.render("pages/success")
 })
 
-app.get('/dashboard', checkAuthenticated, (req, res)=>{
-    let user = req.user;
-    res.render('dashboard', {user});
-})
+app.get("/google",passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-app.get('/protectedroute', (req,res)=>{
-    res.render('protectedroute.ejs');
-})
-
-app.get('/logout',(req,res)=>{
-    res.clearCookie('session-token');
-    res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next){
-
-    let token = req.cookies['session-token'];
-
-    let user = {};
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        });
-        const payload = ticket.getPayload();
-        user.name = payload.name;
-        user.email = payload.email;
-        user.picture = payload.picture;
-      }
-      verify()
-      .then(()=>{
-          req.user = user;
-          next();
-      })
-      .catch(err=>{
-          res.redirect('/login')
-      })
-
-}
+app.get("/google/callback",passport.authenticate("google", { failureRedirect: "/failed" }),
+  function(req,res){
+      res.redirect("/success");
+    }  
+);
 
 
 app.listen(PORT,() => {
