b437ec7f1cef9484c754dd4e6bfc03654069b666
simran1002 <simran2131056@akgec.ac.in>
2022-12-16 15:04:33 +0530

    updated files

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 require("dotenv").config();
 
+const nodemailer = require("nodemailer");
 const express = require("express");
-// const nodemailer = require("nodemailer");
 const path = require("path");
 const ejs = require("ejs");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
 const Razorpay = require("razorpay");
 const cors = require("cors");
-const { nextTick } = require("process");
-const { updateMany, updateOne, update } = require("./source/model/schema");
 const jwt=require("jsonwebtoken");
 const cookieParser=require("cookie-parser");
-const app = express()
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 const session = require("express-session");
 const schema = require("./source/model/schema");
 require("./source/db/connection");
+require("./passport-setup");
+const app = express()
+app.use(express.json());
+app.use(cors());
 
-
-const nodemailer = require("nodemailer");
  
 const CLIENT_ID = "277985285628-560of7ar2drp5dl57go9sphegm3int6n.apps.googleusercontent.com";
  
@@ -45,26 +45,6 @@ app.use(express.static(static_path ))
 app.use(passport.initialize());
 app.use(passport.session());
 app.set("view engine", "ejs");
-//app.set("views", views_path);
-
-require("./passport-setup");
-
-// app.use(
-//   cookieSession({
-//     name: "interactive-session",
-//     keys: ["key1", "key2"],
-//   })
-// );
-// Authentication configuration
-
-// After you declare "app"
-// app.use(session({ secret: 'melody hensley is my spirit animal' }));
-//google auth
-// const {OAuth2Client} = require('google-auth-library');
-// const { Passport } = require('passport');
-// const { Router } = require('express');
-// const CLIENT_ID = '277985285628-560of7ar2drp5dl57go9sphegm3int6n.apps.googleusercontent.com'
-// const client = new OAuth2Client(CLIENT_ID);
 
 
 app.get('/',(req,res)=> {
@@ -72,14 +52,27 @@ app.get('/',(req,res)=> {
     res.render("pages/index")
 })
 
+
 app.get('/pages/login',(req,res) => {
     res.render('pages/login');
 })
 
+app.get("/google",passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+
+app.get("/google/callback",passport.authenticate("google", { failureRedirect: "/failed" }),
+    function(req,res){
+      if (req.user.email.match(/[A-Za-z0-9]+@akgec\.ac\.in/g)) {
+        res.redirect("/success");
+      } else {
+        res.send("Login using college email only")
+      }
+    }  
+);
+
+
 app.get('/success',(req,res) => {
-  // console.log(req.user.email)
-  // console.log(req.user.name.givenName + " " + req.user.name.familyName)
-  // console.log(req.user.picture)
   if(req.isAuthenticated()) {
     const userDetails = {
       email: req.user.email,
@@ -89,25 +82,9 @@ app.get('/success',(req,res) => {
     res.render("pages/register", {userDetailsNew: userDetails})
   } else {
     res.redirect("/")
-  }
-    // console.log(req.user)
-    // res.render("pages/register")
+  }   
 })
 
-app.get("/google",passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get("/google/callback",passport.authenticate("google", { failureRedirect: "/failed" }),
-    function(req,res){
-      if (req.user.email.match(/[A-Za-z0-9]+@akgec\.ac\.in/g)) {
-        res.redirect("/success");
-      } else {
-        res.send("Login using college email only")
-      }
-      // res.redirect("/success");
-     // res.send("success")
-    }  
-);
 
 function loggedIn(req, res, next) {
   if (req.user) {
@@ -117,7 +94,6 @@ function loggedIn(req, res, next) {
   }
 }
 
-// app.get("/test", )
 
 async function sendEmail(toAddress) {
   try {
@@ -149,11 +125,12 @@ async function sendEmail(toAddress) {
   }
 }
 
+
+
 app.post("/add/registered/user",async (req,res,next)=>{
   console.log(req.body)
     
-  // console.log(req.body)
-  // console.log(req.body.email)
+  
   try {
     const userExist=await schema.findOne({email:req.body.email});
     
@@ -173,18 +150,6 @@ app.post("/add/registered/user",async (req,res,next)=>{
       console.log(userData);
       console.log("registered Successfully");
       
-      // let info=await emailService.sendMail({
-      //   from:"simranyadav464@gmail.com",
-      //   to:[req.body.email],
-      //   subject:"Registered Successfully",
-      //   html:`email:${req.body.email} <br>
-      //   userName:${req.body.uname}, <br>
-      //   pictureURL:${req.body.pname}, <br>
-      //   branch:${req.body.bname}, <br>
-      //   year:${req.body.yname}, <br>
-      //   student_number:${req.body.sno},<br>
-      //   roll_number:${req.body.srno},<br>`,
-      // })
       console.log("email send");
       res.redirect("/payment");
     }
@@ -203,9 +168,8 @@ app.post("/add/registered/user",async (req,res,next)=>{
     console.log(e);
     res.status(400).json({message:"Details missing"});
   }
-
-
 })
+
 
 app.get("/payment",loggedIn,(req,res,next)=>{
   try {
@@ -217,77 +181,48 @@ app.get("/payment",loggedIn,(req,res,next)=>{
   }
 })
 
-app.post("/payments",loggedIn, async (req, res,next) => {
-  try {
-    let instance = new Razorpay({
-      key_id: process.env.KEY_ID,
-      key_secret: process.env.KEY_SECRET,
-    });
 
-    let order = await instance.orders.create({
-      amount: 50000,
-      currency: "INR",
-    });
-    res.status(201).json({ success: true, order });
-  } catch (e) {
-    console.log(e);
-  }
-});
+app.post("/payments", async (req,res) => {
+  let { amount } = req.body;
 
-app.post("/paymentverification",loggedIn,async (req,res,next)=>{
+  var instance = new Razorpay({
+    key_id: "rzp_test_42HT4PuykzwauI",
+    key_secret: "12Z6kNBiGKJbbi4HLkZWh6sT",
+  });
+  let order = await instance.orders.create({
+    amount: 50000,
+    currency: "INR",
+    receipt: "receipt#1",
+  });
+  res.status(201).json({
+    success: true,
+    order,
+    amount,
+  });
+  });
 
-  verifyytoken=req.cookies.email;
-  const userver=await jwt.verify(verifyytoken,"done");
-
-  const update_details=userver.email;
-
-
-    const paymentID=req.body.razorpay_payment_id;
-    const orderID=req.body.razorpay_order_id;
-    const signatureID=req.body.razorpay_signature;
-
-    let body =orderID +"|" +paymentID;
-
-  let crypto = require("crypto");
-  let expectedSignature = crypto.createHmac("sha256", process.env.KEY_SECRET).update(body.toString()).digest("hex");
-  console.log("sign received ", signatureID);
-  console.log("sign generated ", expectedSignature);
-  var response = " Signature is false" ;
-  if (expectedSignature === signatureID){
-    const newdata=await Register.updateOne({email:update_details},{
-      payment_status:"true",
-      order_id:orderID,
-      payment_id:paymentID,
-    });
-
-    let info=await transpoter.sendMail({
-      from:process.env.EMAIL_FROM,
-      to:[update_details,`${process.env.EMAIL_USER}`],
-      subject:"Payment Successfull",
-      html:"<h1>Payment Successfull</h1>"
-    })
-    res.redirect("/success");
-  }else{
-    res.status(404).json({message:"Payment failed"});
-  }
-});
-
-
-
-app.get("/success",(req,res)=>{
-  try {
-
-    res.render("payment successfull");
-
-    
-  } catch (error) {
-    res.statusCode(400).json({mesaage:"something went wrong"});
-  }
-})
-
-
-
+  app.post("/verify", async (req, res) => {
+    try {
+      const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+        req.body;
+      const sign = razorpay_order_id + "|" + razorpay_payment_id;
+      const expectedSign = crypto
+        .createHmac("sha256", process.env.KEY_SECRET)
+        .update(sign.toString())
+        .digest("hex");
+  
+      if (razorpay_signature === expectedSign) {
+        return res.status(200).json({ message: "Payment verified successfully" });
+      } else {
+        return res.status(400).json({ message: "Invalid signature sent!" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error!" });
+      console.log(error);
+    }
+  });
+  
 
 app.listen(PORT,() => {
     console.log(`App is running at Port ${PORT}`);
-})
+});
