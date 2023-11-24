1666d59eff50e8859dded80a897d1feeafbb7e8e
Simran Yadav <simranyadav464@gmail.com>
2023-11-24 13:16:11 +0530

    Added the updated code

--- app.js
+++ app.js
@@ -25,131 +25,130 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.post('/register',async (req, res) => {
+app.post('/sendverificationemail', async (req, res) => {
   try {
-    const { name, email, gender, age,password, phoneNumber } = req.body;
+    const { email } = req.body;
 
-    // Validate the input data (add more validation as needed)
-    if (!name || !email || !gender || !age || !password || !phoneNumber) {
-      return res.status(400).json({ error: 'All fields are required' });
-    }
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
 
-    // Check if the user already exists (e.g., based on email or AadharNumber)
-    const existingUser = await User.findOne({ $or: [{ email }, { phoneNumber }] });
     if (existingUser) {
-      return res.status(409).json({ error: 'User with the same email or phoneNumber already exists' });
+      return res.status(409).json({ error: 'User with the same email already exists' });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 8);
+    // Generate a random OTP
+    const otp = otpGenerator.generate(6, { upperCase: false, specialChars: false });
+    const OTPEntry = new OTPModel({ email, OTP: otp });
+    await OTPEntry.save();
 
-    const Schema = new User({ name, email,gender, password: hashedPassword, age , phoneNumber});
-    await Schema.save();
-
-    // Validate the email
-    if (!email) {
-      return res.status(400).json({ error: 'Email is required' });
-    }
-  const smtpConfig = {
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  };
-  
-  // Create a Nodemailer transporter
-  const transporter = nodemailer.createTransport(smtpConfig);
-  
-  // Generate a random OTP
-  const otp = otpGenerator.generate(6, { upperCase: false, specialChars: false });
-  const OTP = new OTPModel({ email, OTP: otp });
-  await OTP.save();
-  
-  // Email content
-  const mailOptions = {
-    from: process.env.SMTP_USER,
-    to: email,
-    subject: 'The OTP verification code for your email',
-    text: `Your OTP of E-mail verification is: ${otp}`,
-  };
-  
-  // Send email with the OTP
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.error(error);
-    }
-    console.log('Email sent: ' + info.response);
-  });
-
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Registration failed' });
-      console.error('User registration error:', error);
+    // Email content
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: 'OTP verification code for your email',
+      text: `Your OTP for email verification is: ${otp}`,
     };
-  });
 
-function generateToken(user) {
-    const payload = { userId: user._id, email: user.email }; // Use user.email instead of User.email
-    const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1h' });
-    return token;
-}
+    // Create a Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
 
-//user login details
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const UserID = await User.findOne({ email });
-      if (!UserID) {
-        return res.status(401).json({ message: 'Login Successfully not' });
+    // Send email with the OTP
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(500).json({ error: 'Error sending verification email' });
       }
-  
-      const passwordMatch = await bcrypt.compare(password, UserID.password);
-      if (!passwordMatch) {
-        const err = new Error('Enter the correct password');
-        err.status = 401;
-        throw err;
-      };
-  
-      const token = generateToken(UserID);
-      console.log("token->",token);
-      res.status(200).json({ message: 'Login successful', token });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+      console.log('Email sent: ' + info.response);
+      res.status(200).json({ message: 'Verification email sent successfully' });
+    });
+  } catch (error) {
+    console.error('Error sending verification email:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-// Route to verify OTP
-app.post('/verifyotp', async (req, res) => {
-  const { email } = req.body;
-  const { enteredOTP } = req.body;
-
+// Verify OTP and save user data
+app.post('/verifyemailandregister', async (req, res) => {
   try {
-    // Retrieve the stored OTP data using Mongoose
-    const otp_Data = await OTPModel.findOne({ email });
-    console.log(otp_Data);
+    const { email, enteredOTP, name, gender, age, password, phoneNumber } = req.body;
 
-    if (!otp_Data) {
+    // Retrieve the stored OTP data using Mongoose
+    const otpData = await OTPModel.findOne({ email });
+
+    if (!otpData) {
       return res.status(400).json({ message: 'OTP not found' });
     }
 
     // Check if the OTP has expired
-    if (Date.now() > otp_Data.expiresAt) {
+    if (Date.now() > otpData.expiresAt) {
       return res.status(400).json({ message: 'OTP has expired' });
     }
 
-    if (enteredOTP === otp_Data.OTP) {
+    if (enteredOTP === otpData.OTP) {
       // If the OTP is verified, you should delete the OTP record from the database
-      await otp_Data.deleteOne({ email });
-      res.status(200).json({ message: 'Your OTP verified successfully' });
+      await otpData.deleteOne({ email });
+
+      // Check if the user already exists
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(409).json({ error: 'User with the same email already exists' });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 8);
+
+      // Create a new user instance
+      const newUser = new User({ name, email, gender, password: hashedPassword, age, phoneNumber });
+
+      // Save the new user to the database
+      await newUser.save();
+
+      res.status(200).json({ message: 'Your email is verified and user registered successfully' });
     } else {
-      res.status(401).json({ message: 'OTP verified successfully' });
+      res.status(401).json({ message: 'Invalid OTP' });
     }
   } catch (error) {
-    console.error('Error verifying OTP:', error);
+    console.error('Error verifying email and saving user data:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+ 
+function generateToken(user) {
+  const payload = { userId: user._id, email: user.email }; // Use user.email instead of User.email
+  const token = jwt.sign(payload, process.env.JWT_KEY, { expiresIn: '1h' });
+  return token;
+}
+
+//user login details
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const UserID = await User.findOne({ email });
+    if (!UserID) {
+      return res.status(401).json({ message: 'Login Successfully not' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, UserID.password);
+    if (!passwordMatch) {
+      const err = new Error('Enter the correct password');
+      err.status = 401;
+      throw err;
+    };
+
+    const token = generateToken(UserID);
+    console.log("token->",token);
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
